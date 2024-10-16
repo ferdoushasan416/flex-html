d348a0689ede53c322f69d004b6c12373626c651
faisal <faisaldevbhaluka@gmail.com>
2024-10-16 22:33:58 +0600

    now one page design in html and css flex box and grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,34 +35,41 @@ $(window).on('scroll', function () {
 
         });
 
+      
+      
 
-        $(document).ready(function(){
-          $('.slider-active').slick({
-            prevArrow: '<button type="button" class="slick-prev"><svg width="30px" class="icon flat-color" data-name="Flat Color" viewBox="0 0 24 24"><path d="M21 11H5.41l1.3-1.29a1 1 0 0 0-1.42-1.42l-3 3a1 1 0 0 0 0 1.42l3 3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42L5.41 13H21a1 1 0 0 0 0-2Z" style="fill:currentColor"/></svg></button>',
-            nextArrow: '<button type="button" class="slick-next"><svg  width="30px" class="icon flat-color" data-name="Flat Color" viewBox="0 0 24 24"><path d="m21.71 11.29-3-3a1 1 0 0 0-1.42 1.42l1.3 1.29H3a1 1 0 0 0 0 2h15.59l-1.3 1.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l3-3a1 1 0 0 0 0-1.42Z" style="fill:currentColor"/></svg></button>',
-            autoplay:false,
-            loop:true,
-            dots:true,
-          });
-         
-      });
-
-     
-
-
-
-
-/* magnificPopup img view */
-$('.popup-image').magnificPopup({
-	type: 'image',
-	gallery: {
-	  enabled: true
-	}
-});
-
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-	type: 'iframe'
+$(document).ready(function(){
+    $('.text-carousel-wrap').slick({
+      autoplay:true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplaySpeed: 2000,
+      centerMode: true,
+      focusOnSelect: true,
+      loop:true,
+      dots:false,
+      arrows:false,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
 });
 
 
